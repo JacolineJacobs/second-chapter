ca7c3f5c9759b94ccdfd6387e498cc573f1cd3f1
JacolineJacobs <150950361+JacolineJacobs@users.noreply.github.com>
2024-11-11 12:07:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// burger menu expand
+
+const burger = document.querySelector('.burger');
+const sidebar = document.querySelector('.sidebar');
+
+function expandSidebar() {
+    sidebar.style.display = "block";
+} 
+
+// burger menu close 
+
+const close = document.querySelector('.close');
+
+function closeSidebar() {
+    sidebar.style.display = "none";
+}
+
